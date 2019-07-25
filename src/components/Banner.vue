8ef2053074a08931f43639609c3382e13d75dd45
WilberCode <parionahuaraca@gmail.com>
2019-07-24 21:14:37 -0500

    se modifico el cv

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,7 +9,7 @@
         <p>Técnico profesional de Informática y Sistemas. Soy una persona apasionado por el maravilloso mundo del desarrollo Web y tengo la capacidad de desarrollar páginas Web responsive, páginas personalizadas con Wordpress, tiendas online con WooCommerce y también tengo conocimiento intermedio de desarrollo en el Backend. Dispuesto a investigar nuevas funcionalidades de la web y aprender, asumir nuevos retos y superarme como profesional.</p>
       </div>
       
-      <v-btn class="banner-btn" round color="#ffffff" data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true" href="https://drive.google.com/file/d/1qzXLQbas_jWJAhXZ9JDnm3sb1p7GHpzc/view" target="_blank" >Descargar CV </v-btn>
+      <v-btn class="banner-btn" round color="#ffffff" data-aos="fade-up"  data-aos-offset="200"  data-aos-once="true" href="https://drive.google.com/file/d/1HtHsHc4pxJf7QvyRH0tbGRQkEpU4vhkS/view" target="_blank" >Descargar CV </v-btn>
       <v-btn class="banner-btn  banner-btn--whatsapp" dark="false" round  color="#00e676"    outline data-aos="fade-up"  data-aos-offset="200" data-aos-delay="300" data-aos-once="true" href="https://api.whatsapp.com/send?phone=51995871015&text=Hola" target="_blank" >Enviar mensaje </v-btn>
     </v-flex>
     <v-flex xs12 sm12 md8 lg8 xl8>  
