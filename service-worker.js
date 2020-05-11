a19cf4ce4e69678ced2231d7eac25c521b2b0d31
WilberCode <parionahuaraca@gmail.com>
2020-05-10 19:42:57 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7112e73b22f18e43ad356438ba37950f.js"
+  "/porfolio/precache-manifest.dc6c18aad5372e563ffc705fa9b6fe42.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "porfolio"});
